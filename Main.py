6829418040de81501341c32fa8032addda7545eb
BmSrv <jmspon33@gmail.com>
2025-03-20 23:18:55 +0900

    pdfToMarkDown, pdfParse

--- Main.py
+++ Main.py
@@ -7,19 +7,22 @@ import pathlib
 # llama_docs = llama_reader.load_data("./자료집_20240226_좌담회_송파세모녀10주기.pdf")
 # print(llama_docs)
 
+# md_text = pymupdf4llm.to_markdown(
+#     doc="./자료집_20240226_좌담회_송파세모녀10주기.pdf",
+#     write_images=True,
+#     page_chunks=True
+# )
+# for current in md_text:
+#     pprint(current)
+#     print("==========================================")
+
+
 md_text = pymupdf4llm.to_markdown(
     doc="./자료집_20240226_좌담회_송파세모녀10주기.pdf",
     write_images=True,
-    # page_chunks=True
 )
-# print(len(md_text))
-# for current in md_text:
-#     if len(current["images"]) != 0:
-#         pprint(current)
-#
-#     print("==========================================")
-#
-# print(md_text)
+
 pathlib.Path("output.md").write_bytes(md_text.encode())
+
 
 # print(md_text)
--- Main_.py
+++ Main_.py
@@ -3,11 +3,35 @@ import requests
 import pathlib
 import pymupdf4llm
 from confluent_kafka import Consumer, KafkaException
+from pprint import pprint
+
+
+def pdfToMarkDown(uuid,file_path):
+    md_text = pymupdf4llm.to_markdown(
+        doc=file_path,
+        write_images=True
+    )
+    # Markdown 내용 출력 및 저장
+    md_output_path = f"{uuid}.md"
+    pathlib.Path(md_output_path).write_text(md_text, encoding="utf-8")
+    print(f"Markdown extracted and saved: {md_output_path}")
+    print(md_text)
+
+def pdfParse(uuid,file_path):
+    md_text = pymupdf4llm.to_markdown(
+        doc=file_path,
+        write_images=True,
+        page_chunks=True
+    )
+    for current in md_text:
+        pprint(current)
+        print("==========================================")
+
 
 consumer_config = {
     'bootstrap.servers': '192.168.0.7:9092',
     'group.id': 'new_test_consumer_group',
-    'auto.offset.reset': 'earliest'
+    'auto.offset.reset': 'latest'
 }
 
 consumer = Consumer(consumer_config)
@@ -25,56 +49,39 @@ try:
             raise KafkaException(msg.error())
         message_value = msg.value().decode('utf-8')
         print(f"Received message: {message_value}")
+        message_json = None
         try:
             message_json = json.loads(message_value)
-            uuid = message_json.get("document", {}).get("file")
-
-            if uuid:
-                file_url = f"http://localhost:8082/file/{uuid}"
-                print(f"Sending request to: {file_url}")
-
-                # HTTP 요청 전송
-                response = requests.get(file_url)
-
-                if response.status_code == 200:
-                    print(f"File request successful: {file_url}")
-
-                    file_link = response.text.strip().strip('"')
-                    print(f"File Download Link: {file_link}")
-
-                    # 파일 다운로드
-                    response = requests.get(file_link)
-                    if response.status_code == 200:
-                        file_path = f"{uuid}.pdf"
-                        with open(file_path, "wb") as file:
-                            file.write(response.content)
-                        print(f"File downloaded: {file_path}")
-
-                        # PDF를 Markdown으로 변환
-                        md_text = pymupdf4llm.to_markdown(
-                            doc=file_path,
-                            write_images=True
-                        )
-
-                        # Markdown 내용 출력 및 저장
-                        md_output_path = f"{uuid}.md"
-                        pathlib.Path(md_output_path).write_text(md_text, encoding="utf-8")
-                        print(f"Markdown extracted and saved: {md_output_path}")
-                        print(md_text)
-
-                    else:
-                        print(f"Failed to download file from link: {file_link}")
-
-                else:
-                    print(f"File request failed with status {response.status_code}: {file_url}")
-            else:
-                print("UUID not found in message.")
-
         except json.JSONDecodeError:
             print("Failed to parse JSON message.")
+            continue
 
+        uuid = message_json.get("document", {}).get("file")
+        file_url = f"http://localhost:8082/file/{uuid}"
+        print(f"Sending request to: {file_url}")
+        # HTTP 요청 전송
+        response = requests.get(file_url)
+        if response.status_code != 200:
+            print(f"File request failed with status {response.status_code}: {file_url}")
+            continue
+        print(f"File request successful: {file_url}")
+        file_link = response.text.strip().strip('"')
+        print(f"File Download Link: {file_link}")
+        # 파일 다운로드
+        response = requests.get(file_link)
+        if response.status_code != 200:
+            print(f"Failed to download file from link: {file_link}")
+            continue
+        file_path = f"{uuid}.pdf"
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+        print(f"File downloaded: {file_path}")
+        # pdfToMarkDown(uuid, file_path)
+        pdfParse(uuid, file_path)
 
 except KeyboardInterrupt:
     print("\nStopping consumer...")
 finally:
     consumer.close()
+
+

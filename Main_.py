37b57353812054d715ca5a91db702185538b77be
BmSrv <jmspon33@gmail.com>
2025-03-22 19:53:30 +0900

    pdf 파싱 및 이벤트 publish 구성

--- Main_.py
+++ Main_.py
@@ -1,32 +1,16 @@
 import json
+from datetime import datetime
 import requests
-import pathlib
 import pymupdf4llm
-from confluent_kafka import Consumer, KafkaException
-from pprint import pprint
+from confluent_kafka import Consumer, KafkaException, Producer
+from ollama import Client
 
+ollama = Client(host='http://192.168.0.8:11434')
 
-def pdfToMarkDown(uuid,file_path):
-    md_text = pymupdf4llm.to_markdown(
-        doc=file_path,
-        write_images=True
-    )
-    # Markdown 내용 출력 및 저장
-    md_output_path = f"{uuid}.md"
-    pathlib.Path(md_output_path).write_text(md_text, encoding="utf-8")
-    print(f"Markdown extracted and saved: {md_output_path}")
-    print(md_text)
-
-def pdfParse(uuid,file_path):
-    md_text = pymupdf4llm.to_markdown(
-        doc=file_path,
-        write_images=True,
-        page_chunks=True
-    )
-    for current in md_text:
-        pprint(current)
-        print("==========================================")
-
+producer_config = {
+    'bootstrap.servers': '192.168.0.7:9092'  # Kafka 브로커 주소
+}
+producer = Producer(producer_config)
 
 consumer_config = {
     'bootstrap.servers': '192.168.0.7:9092',
@@ -35,29 +19,95 @@ consumer_config = {
 }
 
 consumer = Consumer(consumer_config)
-topic = 'docuhelper-api'
+consumerTopic = 'docuhelper-api'
 
-consumer.subscribe([topic])
+consumer.subscribe([consumerTopic])
+
+
+def publishEvent(topic, key, value):
+    event_key = {
+        "eventType": key,
+        "eventPublishDt": list(datetime.utcnow().timetuple()[:6]) + [int(datetime.utcnow().microsecond * 1000)]
+    }
+
+    print(f"Publish Event : {event_key}")
+
+    producer.produce(
+        topic=topic,
+        key=json.dumps(event_key),
+        value=json.dumps(value),
+        callback=lambda err, msg: print("전송 성공" if err is None else f"에러 발생: {err}")
+    )
+
+
+def pdfParse(document_uuid, document_file_uuid, file_path):
+    document_parse_start_event = {
+        "documentUuid": document_uuid
+    }
+
+    publishEvent("docuhelper-document-parser", "DocumentParseStart", document_parse_start_event)
+
+    md_text = pymupdf4llm.to_markdown(
+        doc=file_path,
+        page_chunks=True,
+        # write_images=True,
+    )
+    for current in md_text:
+        page = current['metadata']['page']
+        pageContent = current["text"]
+        pageEmbdRes = ollama.embeddings(model='bge-m3', prompt=pageContent).embedding
+
+        document_parse_event = {
+            "documentUuid": document_uuid,
+            "page": page,
+            "content": pageContent,
+            "embedContent": pageEmbdRes
+        }
+
+        publishEvent("docuhelper-document-parser", "DocumentParse", document_parse_event)
+
+    document_parse_complete_event = {
+        "documentUuid": document_uuid
+    }
+
+    publishEvent("docuhelper-document-parser", "DocumentParseComplete", document_parse_complete_event)
+
+
+def getEventValue(msg):
+    message_value = msg.value().decode('utf-8')
+    print(f"Received Event Value: {message_value}")
+    message_json = json.loads(message_value)
+    return message_json
+
+
+def getEventKey(msg):
+    key = msg.key().decode('utf-8')
+    print(f"Received Event Key: {key}")
+    key_json = json.loads(key)
+    return key_json
+
 
 try:
-    print(f"Consuming messages from topic: {topic}")
+    print(f"Consuming messages from topic: {consumerTopic}")
     while True:
-        msg = consumer.poll(timeout=0)  # 메시지를 1초 동안 기다림
+        msg = consumer.poll(timeout=0)
         if msg is None:
             continue
         if msg.error():
             raise KafkaException(msg.error())
-        message_value = msg.value().decode('utf-8')
-        print(f"Received message: {message_value}")
+
+        key_json = None
         message_json = None
         try:
-            message_json = json.loads(message_value)
+            key_json = getEventKey(msg)
+            message_json = getEventValue(msg)
         except json.JSONDecodeError:
             print("Failed to parse JSON message.")
             continue
 
-        uuid = message_json.get("document", {}).get("file")
-        file_url = f"http://localhost:8082/file/{uuid}"
+        document_uuid = message_json.get("document", {}).get("uuid")
+        document_file_uuid = message_json.get("document", {}).get("file")
+        file_url = f"http://localhost:8082/file/{document_file_uuid}"
         print(f"Sending request to: {file_url}")
         # HTTP 요청 전송
         response = requests.get(file_url)
@@ -72,16 +122,14 @@ try:
         if response.status_code != 200:
             print(f"Failed to download file from link: {file_link}")
             continue
-        file_path = f"{uuid}.pdf"
+        file_path = f"./pdf/{document_file_uuid}.pdf"
         with open(file_path, "wb") as file:
             file.write(response.content)
         print(f"File downloaded: {file_path}")
-        # pdfToMarkDown(uuid, file_path)
-        pdfParse(uuid, file_path)
+
+        pdfParse(document_uuid, document_file_uuid, file_path)
 
 except KeyboardInterrupt:
     print("\nStopping consumer...")
 finally:
     consumer.close()
-
-
